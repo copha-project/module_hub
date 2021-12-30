e9a5cc7ab6a8dcdc1fa10ccbc6988906725dfa1a
aong <yxwzaxns@gmail.com>
2021-12-30 18:17:28 +0800

    use octokit

--- src/server/modules/repository/remote.ts
+++ src/server/modules/repository/remote.ts
@@ -1,19 +1,27 @@
-import Utils from 'uni-utils'
 import Repository from "../../../class/repository"
 import { Module } from "../model"
 import { Octokit } from "@octokit/core"
-
 export default class RemoteRepository extends Repository implements Repository {
     private db: Module[] = []
     private sha: string = ""
+    private githubAPI = new Octokit({auth: this.appConfig.GithubToken})
+
+    private moduleMeta = {
+        owner: "copha-project",
+        repo: "module-database",
+        path: "modules.json",
+    }
 
     async init(){
         try {
             await super.loadStorageList()
-            const modulesData = await Utils.download(this.dbConfig.RemoteSource,{})
-            const jsonData = JSON.parse(modulesData)
-            this.sha = jsonData.sha
-            this.db = JSON.parse(Buffer.from(jsonData.content,'base64').toString('utf-8'))
+            // const modulesData = await Utils.download(this.dbConfig.RemoteSource,{})
+            const resp = await this.githubAPI.request("GET /repos/{owner}/{repo}/contents/{path}", {
+                ...this.moduleMeta
+            })
+            const modulesData = resp.data as {content:string, sha: string}
+            this.sha = modulesData.sha
+            this.db = JSON.parse(Buffer.from(modulesData.content,'base64').toString('utf-8'))
         } catch (error:any) {
             this.log.err(`get db error: ${error.message}`)
         }
@@ -31,7 +39,7 @@ export default class RemoteRepository extends Repository implements Repository {
         const editModule = this.db.find(e=>e.name === module.name)!
         editModule.packages = []
         const content = Buffer.from(JSON.stringify(this.db, undefined, 4)).toString('base64')
-        return this.save(content)
+        return this.sync(content)
     }
 
     delete(id: string): Promise<boolean> {
@@ -42,12 +50,9 @@ export default class RemoteRepository extends Repository implements Repository {
         return this.db
     }
 
-    private async save(content:string){
-        const octokit = new Octokit({auth: this.appConfig.GithubToken})
-        const resp = await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
-            owner: "copha-project",
-            repo: "module-database",
-            path: "modules.json",
+    private async sync(content:string){
+        const resp = await this.githubAPI.request("PUT /repos/{owner}/{repo}/contents/{path}", {
+            ...this.moduleMeta,
             branch: "main",
             message: "update module data",
             content: content,
