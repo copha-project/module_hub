a53ba2698a666d4e16cb6843f794e9403d3a51f7
yxwzaxns <yxwzaxns@gmail.com>
2022-02-07 23:00:19 +0800

    remove useless code

--- src/server/home/validators.ts
+++ src/server/home/validators.ts
@@ -1,17 +1,5 @@
 import Joi from 'joi'
 
-export const createModule = Joi.object({
-    name: Joi.string().trim().required(),
-    desc: Joi.string().required(),
-    type: Joi.string().required(),
-    repository: Joi.string().required()
-})
-
-export const updateModule = Joi.object({
-    desc: Joi.string().required(),
-    repository: Joi.string().required()
-})
-
 export const revealToken = Joi.object({
     id: Joi.string().required()
 })
