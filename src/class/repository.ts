a3dcb08d54e9e63137f8f5fa925010e5fee6a9fa
aong <yxwzaxns@gmail.com>
2022-02-16 16:01:31 +0800

    fix bug of return item

--- src/class/repository.ts
+++ src/class/repository.ts
@@ -2,6 +2,8 @@ import Base from "./base"
 interface BaseModel {
     id: string
 }
+
+const clone = (obj: any) => JSON.parse(JSON.stringify(obj))
 export default class Repository extends Base {
     static errors = {
         noDocSelected : new Error("no doc selected"),
@@ -30,7 +32,7 @@ export default class Repository extends Base {
         return this
     }
 
-    async all<T>(): Promise<T[]> {
+    async all<T>(): Promise<Readonly<T[]>> {
         if(!this.db[this.useDoc]) throw this.errors.noData
         return this.db[this.useDoc]
     }
@@ -38,27 +40,27 @@ export default class Repository extends Base {
     async findById<T>(id: string): Promise<T> {
         const item = this.currentDoc.find(e => e.id === id)
         if (!item) throw this.errors.itemNotFound
-        return item
+        return clone(item)
     }
 
-    async findArrBy<T>(value: string, key?: string): Promise<T[]> {
+    async findArrBy<T>(value: string, key?: string): Promise<Readonly<T[]>> {
         return this.currentDoc.filter(e => e[key||'id'] === value)
     }
 
     async findBy<T>(value: string, key: string): Promise<T> {
         const item = this.currentDoc.find(e => e[key] === value)
         if (!item) throw this.errors.itemNotFound
-        return item
+        return clone(item)
     }
 
     async findByName<T>(name: string): Promise<T> {
         const item = this.currentDoc.find(e => e.name === name)
         if (!item) throw this.errors.itemNotFound
-        return item
+        return clone(item)
     }
 
     async add<T extends BaseModel>(item: T): Promise<T> {
-        const cloneDb = Object.assign([], this.currentDoc)
+        const cloneDb = clone(this.currentDoc)
         cloneDb.push(item)
         await this.sync(cloneDb, `add item: ${item?.id || JSON.stringify(item)}`)
         this.setCurrentDoc(cloneDb)
@@ -70,7 +72,7 @@ export default class Repository extends Base {
         const itemIndex = this.currentDoc.findIndex(e => e[itemKey] === item[itemKey])
         if (itemIndex === -1) throw this.errors.itemNotFound
 
-        const cloneDb: any[] = Object.assign([], this.currentDoc)
+        const cloneDb: any[] = clone(this.currentDoc)
         cloneDb[itemIndex] = item
         
         await this.sync(cloneDb, `update item: ${item[itemKey]}`)
@@ -93,7 +95,7 @@ export default class Repository extends Base {
             this.log.info("no item found!")
             return
         }
-        const cloneDb = Object.assign([], this.currentDoc)
+        const cloneDb = clone(this.currentDoc)
         const deletedItem = cloneDb.splice(index, 1)
         const msg = "remove item: " + deletedItem.map((e: any) => e.name || e.id).join(" ")
         await this.sync(cloneDb, msg)
