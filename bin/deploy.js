c3523858442720aa866c110cdf61cb803c83ca2a
aong <yxwzaxns@gmail.com>
2022-03-01 11:00:54 +0800

    add host deploy test action

--- bin/deploy.js
+++ bin/deploy.js
@@ -3,8 +3,10 @@ const fs = require('fs')
 const path = require('path')
 const AdminToken = process.env['MODULE_HUB_APP_SECRET']
 const SSHDir = process.env['HOME'] + '/.ssh'
+const KnownHostsFile = SSHDir + '/known_hosts'
 const APP_PATH = '/var/www/copha-module-hub'
 const HOST_USER = 'ubuntu'
+const PackageHostsUrl = 'https://hub.copha.net/api/v1/package_hosts'
 
 const quit = (msg)=>{
     console.log(msg)
@@ -13,8 +15,8 @@ const quit = (msg)=>{
 
 async function main(){
     const { execa, execaCommandSync } = await import('execa')
-    const task = cmd => {
-        const {stderr, stdout} = execaCommandSync(cmd,{shell: true})
+    const task = (cmd, options={}) => {
+        const {stderr, stdout} = execaCommandSync(cmd,{shell: true, ...options})
         console.log(stdout || stderr)
     }
     const sshExecBuilder = host =>{
@@ -23,47 +25,46 @@ async function main(){
         }
     }
 
-    const PackageHostsUrl = 'https://hub.copha.net/api/v1/package_hosts'
     const hosts = await axios.get(PackageHostsUrl)
     if(hosts.data.code !== 200 || hosts.data.data.length === 0) throw new Error('no hosts found')
     console.log(execaCommandSync('ssh-add -D').stdout)
-    for (const host of  hosts.data.data) {
-        const hostKey = await axios.get(`${PackageHostsUrl}/${host.id}/key`,{
+    for (const host of hosts.data.data) {
+        const hostSecretResp = await axios.get(`${PackageHostsUrl}/${host.id}/secret`,{
             headers: {
                 authorization: AdminToken,
             },
         })
-        if(hostKey.data.code !== 200) throw new Error(`fetch key error: ${hostKey.data.msg || hostKey.data.code}`)
-        // fs.writeFileSync(`${host.host}.key`,hostKey.data.data+'\n')
+        if(hostSecretResp.data?.code !== 200) throw new Error(`fetch host secret info error: ${hostSecretResp.data.msg || hostSecretResp.data.code}`)
+        // fs.writeFileSync(`${host.host}.key`,hostSecret.data.data+'\n')
         // fs.chmodSync(`${host.host}.key`, 0o400)
         // execa.execaCommandSync(`ssh-add ./${host.host}.key`)
-        console.log(`Adding ${host.host} openssh private key`)
-        console.log(execaCommandSync('ssh-add - ', {input: hostKey.data.data+'\n'}).stderr);
-
-        console.log(`Adding host ${host.host} to known_hosts`)
+        const hostSecret = hostSecretResp.data.data.secret
+        console.log(`---- Adding ${host.host} openssh private key`)
+        task('ssh-add - ', {input: hostSecret.key+'\n'})
+        console.log(`---- Adding host ${host.host} to known_hosts`)
         
         const {stdout} = await execa('ssh-keyscan', ['-p', '22', host.host])
-        const knownHostsFile = SSHDir + '/known_hosts'
-        fs.appendFileSync(knownHostsFile, stdout)
+        
+        fs.appendFileSync(KnownHostsFile, stdout)
         // await fs.chmodAsync(knownHostsFile, '644')
-        const sshExec = sshExecBuilder(host)
-        task(`rsync -rvz ./dist ${host.user || HOST_USER}@${host.host}:${APP_PATH}`)
-        task(`scp ./package.json ./yarn.lock ./ecosystem.config.js  ${host.user || HOST_USER}@${host.host}:${APP_PATH}`)
-        sshExec(`cd ${APP_PATH} && yarn`)
-        sshExec(`cd ${APP_PATH} && pm2 start ecosystem.config.js`)
+        hostSecret.host = host.host
+        hostSecret.path = hostSecret.path || APP_PATH
+        const sshExec = sshExecBuilder(hostSecret)
+        const targetPath = `${hostSecret.user || HOST_USER}@${ hostSecret.host }:${ hostSecret.path }`
+        task(`rsync -rvz ./dist ${targetPath}`)
+        task(`scp ./package.json ./yarn.lock ./ecosystem.config.js  ${targetPath}`)
+        sshExec(`cd ${ hostSecret.path } && yarn`)
+        sshExec(`cd ${ hostSecret.path } && pm2 start ecosystem.config.js`)
+        console.log(`${host.host} update ok`)
     }
-    quit("ok")
+    quit("---- All host deploy done")
 }
 
 ;(async()=>{
     process.chdir(path.join(__dirname,'../'))
     try {
-        // execa.execaCommandSync('ssh-add -a /tmp/ssh-auth.sock')
+        execa.execaCommandSync('ssh-add -a /tmp/ssh-auth.sock')
         await main()
-        // const { execa, execaCommandSync } = await import('execa')
-        // console.log(execaCommandSync("ssh ubuntu@150.158.31.232 'cd /var/www/copha-module-hub && yarn'", {
-        //     shell: true
-        // }).stdout);
     } catch (error) {
         quit(error)
     }
