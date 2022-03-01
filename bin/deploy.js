c620926c594593edff79ef3a37b8142f799d9fe8
aong <yxwzaxns@gmail.com>
2022-03-01 11:41:32 +0800

    update action

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,33 +1,31 @@
 const axios = require('axios')
+const core = require('@actions/core')
 const fs = require('fs')
 const path = require('path')
-const AdminToken = process.env['MODULE_HUB_APP_SECRET']
+const AdminToken = process.env['MODULE_HUB_APP_SECRET'] || core.getInput('appKey', { required: true })
 const SSHDir = process.env['HOME'] + '/.ssh'
 const KnownHostsFile = SSHDir + '/known_hosts'
 const APP_PATH = '/var/www/copha-module-hub'
 const HOST_USER = 'ubuntu'
 const PackageHostsUrl = 'https://hub.copha.net/api/v1/package_hosts'
 
-const quit = (msg)=>{
-    console.log(msg)
-    process.exit()
-}
-
 async function main(){
     const { execa, execaCommandSync } = await import('execa')
     const task = (cmd, options={}) => {
         const {stderr, stdout} = execaCommandSync(cmd,{shell: true, ...options})
-        console.log(stdout || stderr)
+        core.info(stdout || stderr)
     }
     const sshExecBuilder = host =>{
         return cmd => {
             task(`ssh ${host.user || HOST_USER}@${host.host} '${cmd}'`)
         }
     }
-
+    
+    core.info("---- start a ssh agent")
+    task('ssh-add -a /tmp/ssh-auth.sock')
     const hosts = await axios.get(PackageHostsUrl)
     if(hosts.data.code !== 200 || hosts.data.data.length === 0) throw new Error('no hosts found')
-    console.log(execaCommandSync('ssh-add -D').stdout)
+    task('ssh-add -D')
     for (const host of hosts.data.data) {
         const hostSecretResp = await axios.get(`${PackageHostsUrl}/${host.id}/secret`,{
             headers: {
@@ -39,9 +37,9 @@ async function main(){
         // fs.chmodSync(`${host.host}.key`, 0o400)
         // execa.execaCommandSync(`ssh-add ./${host.host}.key`)
         const hostSecret = hostSecretResp.data.data.secret
-        console.log(`---- Adding ${host.host} openssh private key`)
+        core.info(`---- Adding ${host.host} openssh private key`)
         task('ssh-add - ', {input: hostSecret.key+'\n'})
-        console.log(`---- Adding host ${host.host} to known_hosts`)
+        core.info(`---- Adding host ${host.host} to known_hosts`)
         
         const {stdout} = await execa('ssh-keyscan', ['-p', '22', host.host])
         
@@ -55,17 +53,16 @@ async function main(){
         task(`scp ./package.json ./yarn.lock ./ecosystem.config.js  ${targetPath}`)
         sshExec(`cd ${ hostSecret.path } && yarn`)
         sshExec(`cd ${ hostSecret.path } && pm2 start ecosystem.config.js`)
-        console.log(`${host.host} update ok`)
+        core.info(`${host.host} update ok`)
     }
-    quit("---- All host deploy done")
+    core.info("---- All host deploy done")
 }
 
 ;(async()=>{
     process.chdir(path.join(__dirname,'../'))
     try {
-        execa.execaCommandSync('ssh-add -a /tmp/ssh-auth.sock')
         await main()
     } catch (error) {
-        quit(error)
+        core.setFailed(`Action failed with error ${error}`)
     }
 })()
